15eb226a2ad7dd6bcd100670078bb9afef424b1f
OSERHIRDEV <oserhir10@gmail.com>
2024-11-07 16:43:54 +0100

    Tailwind CSS Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="">
-      <h1 className="">Hello Word</h1>
+    <div className="  ">
+      <h1 className="  h1-bold">Hello Word</h1>
     </div>
   );
 }
