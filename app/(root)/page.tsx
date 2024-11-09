03c2b0b5581b94dd1aeec8e2abe7d65ac41207f6
OSERHIRDEV <oserhir10@gmail.com>
2024-11-09 12:26:47 +0100

    Create Auth Routes

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+    </>
+  );
+};
+
+export default Home;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="  ">
-      <h1 className="  h1-bold">Hello Word</h1>
-      <p className="  h1-bold">DevOverflow</p>
-      <p className="  h1-bold font-space-grotesk">DevOverflow</p>
-    </div>
-  );
-}
